a407e74ffab6d42b41c411cf1d3479e35f99fb0a
Griffin Muir <gmuir@lcs.on.ca>
2023-10-23 15:41:27 -0400

    Initial Commit

---
+++ Magic8Ball/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Magic8Ball
+//
+//  Created by Griffin Muir on 2023-10-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Magic8Ball/Magic8BallApp.swift
@@ -0,0 +1,17 @@
+//
+//  Magic8BallApp.swift
+//  Magic8Ball
+//
+//  Created by Griffin Muir on 2023-10-23.
+//
+
+import SwiftUI
+
+@main
+struct Magic8BallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
